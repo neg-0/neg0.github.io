92679b53b4b8b7f39881d0c166af310690901a3d
Dustin Stringer <dustin.stringer@spaceforce.mil>
2023-04-26 11:31:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import reactToWebComponent from 'react-to-webcomponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Winds App</h1>
       </header>
     </div>
-  );
+  )
 }
+const windsApp = reactToWebComponent(App, React, ReactDOM);
+customElements.define('winds-app', windsApp);
 
 export default App;
